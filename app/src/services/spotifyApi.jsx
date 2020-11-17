ea2748dd35623aefc4d269ddf7ec42fdbb8c427f
Erik Vlasblom <erik.vlasblom@gmail.com>
2020-11-17 14:47:23 +0100

    Updated api with put and delete requests.

--- app/src/services/spotifyApi.jsx
+++ app/src/services/spotifyApi.jsx
@@ -8,7 +8,7 @@ const ALLOWED_METHODS = ['me', 'me/following', 'me/albums', 'me/tracks'];
 
 // ---------- BASE -------------------------------------------------- 
 
-function _baseApi(access_token, method, params) {
+function _getApi(access_token, method, params) {
   if (!ALLOWED_METHODS.includes(method)) throw new ApiError("Invalid argument selected: method.");
 
   const config = {
@@ -22,7 +22,41 @@ function _baseApi(access_token, method, params) {
     }
   };
 
-  console.log(config.baseURL + config.url)
+  return axios(config)
+}
+
+function _putApi(access_token, method, params) {
+  if (!ALLOWED_METHODS.includes(method)) throw new ApiError("Invalid argument selected: method.");
+
+  const config = {
+    url: method,
+    baseURL: 'https://api.spotify.com/v1/',
+    method: 'PUT',
+    timeout: 4000,
+    json: true,
+    data: params,
+    headers: {
+      'Authorization': 'Bearer ' + access_token
+    }
+  };
+
+  return axios(config)
+}
+
+function _deleteApi(access_token, method, params) {
+  if (!ALLOWED_METHODS.includes(method)) throw new ApiError("Invalid argument selected: method.");
+
+  const config = {
+    url: method,
+    baseURL: 'https://api.spotify.com/v1/',
+    method: 'DELETE',
+    timeout: 4000,
+    json: true,
+    data: params,
+    headers: {
+      'Authorization': 'Bearer ' + access_token
+    }
+  };
 
   return axios(config)
 }
@@ -30,19 +64,19 @@ function _baseApi(access_token, method, params) {
 // ---------- API -------------------------------------------------- 
 
 export function getProfile(access_token, opts) {
-  return _baseApi(access_token, 'me', {})
+  return _getApi(access_token, 'me', {})
 }
 
 export function getFollowingArtists(access_token, opts) { 
   if (!opts) throw new ApiError("Missing required argument: opts.")
-  
+
   const params = {
     type: 'artist',
     after: opts.after,
-    limit: opts.limit
+    limit: opts.limit // min 1 max 50 default 20
   }
 
-  return _baseApi(access_token, 'me/following', params)
+  return _getApi(access_token, 'me/following', params)
 }
 
 export function getSavedAlbums(access_token, opts) {
@@ -52,11 +86,11 @@ export function getSavedAlbums(access_token, opts) {
   const params = {
     type: 'albums',
     offset: opts.offset,
-    limit: opts.limit,
+    limit: opts.limit, // min 1 max 50 default 20
     market: opts.market
   }
 
-  return _baseApi(access_token, 'me/albums', params)
+  return _getApi(access_token, 'me/albums', params)
 }
 
 export function getSavedTracks(access_token, opts) {
@@ -66,11 +100,79 @@ export function getSavedTracks(access_token, opts) {
   const params = {
     type: 'tracks',
     offset: opts.offset,
-    limit: opts.limit,
+    limit: opts.limit, // min 1 max 50 default 20
     market: opts.market
   }
 
-  return _baseApi(access_token, 'me/tracks', params)
+  return _getApi(access_token, 'me/tracks', params)
+}
+
+export function setFollowingArtists(access_token, opts) { 
+  if (!opts) throw new ApiError("Missing required argument: opts.")
+  if (!opts.ids) throw new ApiError("Missing required option: ids.");
+
+  const params = {
+    type: 'artist',
+    ids: opts.ids, // max 50
+  }
+
+  return _putApi(access_token, 'me/following', params)
+}
+
+export function setSavedAlbums(access_token, opts) { 
+  if (!opts) throw new ApiError("Missing required argument: opts.")
+  if (!opts.ids) throw new ApiError("Missing required option: ids.");
+
+  const params = {
+    ids: opts.ids, // max 50
+  }
+
+  return _putApi(access_token, 'me/albums', params)
+}
+
+export function setSavedTracks(access_token, opts) { 
+  if (!opts) throw new ApiError("Missing required argument: opts.")
+  if (!opts.ids) throw new ApiError("Missing required option: ids.");
+
+  const params = {
+    ids: opts.ids, // max 50
+  }
+
+  return _putApi(access_token, 'me/tracks', params)
+}
+
+export function removeFollowingArtists(access_token, opts) { 
+  if (!opts) throw new ApiError("Missing required argument: opts.")
+  if (!opts.ids) throw new ApiError("Missing required option: ids.");
+
+  const params = {
+    type: 'artist',
+    ids: opts.ids, // max 50
+  }
+
+  return _deleteApi(access_token, 'me/following', params)
+}
+
+export function removeSavedAlbums(access_token, opts) { 
+  if (!opts) throw new ApiError("Missing required argument: opts.")
+  if (!opts.ids) throw new ApiError("Missing required option: ids.");
+
+  const params = {
+    ids: opts.ids, // max 50
+  }
+
+  return _deleteApi(access_token, 'me/albums', params)
+}
+
+export function removeSavedTracks(access_token, opts) { 
+  if (!opts) throw new ApiError("Missing required argument: opts.")
+  if (!opts.ids) throw new ApiError("Missing required option: ids.");
+
+  const params = {
+    ids: opts.ids, // max 50
+  }
+
+  return _deleteApi(access_token, 'me/tracks', params)
 }
 
 // ---------- PARSERS -------------------------------------------------- 
