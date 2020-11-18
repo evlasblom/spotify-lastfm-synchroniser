75a66fcac4f18c792f40f6d5930e12c4a2b864d7
Erik Vlasblom <erik.vlasblom@gmail.com>
2020-11-18 14:22:43 +0100

    Added a react scroll hook.

---
+++ app/src/hooks/useScroll.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+
+function useScroll() {
+  const [lastScrollTop, setLastScrollTop] = useState(0);
+  const [bodyOffset, setBodyOffset] = useState(
+    document.body.getBoundingClientRect()
+  );
+  const [scrollY, setScrollY] = useState(bodyOffset.top);
+  const [scrollX, setScrollX] = useState(bodyOffset.left);
+  const [scrollDirection, setScrollDirection] = useState();
+
+  const listener = e => {
+    setBodyOffset(document.body.getBoundingClientRect());
+    setScrollY(-bodyOffset.top);
+    setScrollX(bodyOffset.left);
+    setScrollDirection(lastScrollTop > -bodyOffset.top ? "down" : "up");
+    setLastScrollTop(-bodyOffset.top);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", listener);
+    return () => {
+      window.removeEventListener("scroll", listener);
+    };
+  });
+
+  return {
+    scrollY,
+    scrollX,
+    scrollDirection
+  };
+}
+
+export default useScroll;
